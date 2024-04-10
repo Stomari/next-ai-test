c631942852a4e3dc223b45549da41488f8de53fe
Sérgio <sfestatomari@gmail.com>
2024-04-10 11:18:27 -0300

    add material ui lib, and ai route

---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,20 @@
+import { OpenAIStream, StreamingTextResponse } from "ai";
+import OpenAI from "openai";
+
+export const openAiInstance = new OpenAI({ apiKey: process.env.OPENAI_API_KEY })
+ 
+// IMPORTANT! Set the runtime to edge
+export const runtime = 'edge';
+
+export async function POST(req: Request) {
+  const { messages } = await req.json()
+  const response = await openAiInstance.chat.completions.create({
+    model: 'gpt-3.5-turbo',
+    stream: true,
+    messages,
+  })
+ 
+  const stream = OpenAIStream(response)
+ 
+  return new StreamingTextResponse(stream)
+}
